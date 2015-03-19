a0a39406dad58879224955ebf24842e4eea3a0f1
honorabrutroll <edward.yang6771@gmail.com>
2015-03-19 16:16:08 -0500

    buildall.bat fixes

--- buildall.bat
+++ buildall.bat
@@ -1,18 +1,20 @@
+@ECHO OFF
 cd %~dp0
 :CheckOS
 IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
 
 :64BIT
-set distver="Windowsx86_64"
+set distver=Windowsx86_64
 GOTO END
 
 :32BIT
-set distver="Windowsx86"
+set distver=Windowsx86
 GOTO END
 
 :END
-md dist
-rd /s /q dist/%DISTVER%
-copy %~dp0src/curl.exe ./dist/%DISTVER%/curl.exe
-ghc --make %~dp0src/client.hs -O2 -H500m -o ./dist/%DISTVER%/client
-ghc --make %~dp0src/server.hs -O2 -H500m -o ./dist/%DISTVER%/server
+@ECHO ON
+rd /s /q dist\%DISTVER%
+md dist\%DISTVER%
+copy /b src\curl.exe dist\%DISTVER%\curl.exe /b
+ghc --make src\client.hs -O2 -H500m -o dist\%DISTVER%\client.exe
+ghc --make src\server.hs -O2 -H500m -o dist\%DISTVER%\server.exe
